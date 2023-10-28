be03219873e6d6597e1426c336eb9e912aed2105
yukiny0811 <yukiny0811@gmail.com>
2023-10-28 13:03:51 +0900

    initial

---
+++ SampleViewApp/SampleViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleViewAppApp.swift
+//  SampleViewApp
+//
+//  Created by Yuki Kuwashima on 2023/10/28.
+//
+
+import SwiftUI
+
+@main
+struct SampleViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
